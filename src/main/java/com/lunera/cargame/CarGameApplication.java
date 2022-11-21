f281077d2fddbfe8aca63aa2eef6f38965defbcb
Pintado <alberto.pintado@solera.com>
2022-11-21 11:58:21 +0100

    Initial setup of server with WebSocket dependencies

---
+++ src/main/java/com/lunera/cargame/CarGameApplication.java
@@ -0,0 +1,13 @@
+package com.lunera.cargame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunera/cargame/CarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunera.cargame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
